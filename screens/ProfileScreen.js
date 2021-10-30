d58959576a732898f672f1ea0e813edfc22608ca
joshuapopp17 <54725087+joshuapopp17@users.noreply.github.com>
2021-10-30 11:39:33 -0700

    Add files via upload

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,55 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useContext } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View, TextInput, Pressable, ScrollView } from 'react-native';
+import { Icon } from 'react-native-elements';
+import EventCard from '../components/EventCard';
 
-import { IconButton, InputField } from '../components';
-import Firebase from '../config/firebase';
-import { AuthenticatedUserContext } from '../navigation/AuthenticatedUserProvider';
-
-const auth = Firebase.auth();
-
-export default function ProfileScreen() {
-  const { user } = useContext(AuthenticatedUserContext);
-  const handleSignOut = async () => {
-    try {
-      await auth.signOut();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export default function ProfileScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <StatusBar style='dark-content' />
-        <View style={styles.info}>
-          <Text>Email={user.email}</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.profileContainer}>
+        <View style={styles.profileName}>
+          <Text style={styles.title}>@Poppmane</Text>
+          <Icon name="notifications-outline" type='ionicon' color='black'/>
+          <Icon name="menu-outline" type='ionicon' color='black'/>
         </View>
-        <View style={styles.logOut}>
-          <IconButton
-            name='logout'
-            size={24}
-            color='#000000'
-            onPress={handleSignOut}
-          />
-          <Text>Log out</Text>
+        <View style={styles.profileButtons}>
+          <Pressable style={styles.button} onPress={() => navigation.navigate('Register')}>
+            <Text style={styles.friend}>Add Friend</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => navigation.navigate('Register')}>
+            <Text style={styles.friend}>Edit Profile</Text>
+          </Pressable>
         </View>
-    </View>
+      </View>
+
+      <View style={styles.componentContainer}>
+        <View style={styles.compHeader}>
+          <Text style={styles.title}>Your Events</Text>
+        </View>
+      </View>
+      <ScrollView style={styles.scroll}>
+        <EventCard />
+        <EventCard />
+      </ScrollView>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#EAE7E0',
-    paddingTop: 50,
-    paddingHorizontal: 12,
+    paddingTop: 25,
+    paddingBottom: 60,
+    paddingLeft: 15,
+    paddingRight: 15,
+    marginBottom: 250,
+  },
+  scroll: {
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'left',
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  profileName: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    marginBottom: 5,
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
-  info: {
-    marginBottom: 14
+  profileContainer: {
+    marginBottom: 60,
+    marginTop: 30,
+    justifyContent: 'space-between',
   },
-  logOut: {
-    width: 50,
-    height: 50,
-  }
+  profileButtons: {
+    flexDirection: 'row',
+  },
+  compHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    borderRadius: 8,
+    width: 100,
+    borderColor: 'black',
+    borderWidth: 2,
+    alignItems: 'center',
+    paddingVertical: 5,
+    marginRight: 15
+  },
+  friend: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color:  'black',
+  },
+
+  buttonCreate: {
+    borderRadius: 8,
+    width: 120,
+    alignItems: 'center',
+    paddingVertical: 7,
+    borderColor:  'black',
+    borderWidth: 2,
+    backgroundColor: 'lightgrey',
+  },
+  createText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color:  'black',
+  },
 });
