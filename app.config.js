9824d74983cad7e644faf79e8c6a78577429ec23
joshuapopp17 <54725087+joshuapopp17@users.noreply.github.com>
2021-11-30 00:00:42 -0800

    Add files via upload

--- app.config.js
+++ app.config.js
@@ -1,38 +1,37 @@
 import 'dotenv/config';
 
-export default{
-  "expo": {
-    "name": "firebase-auth",
-    "slug": "firebase-auth",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+export default {
+  expo: {
+    name: 'CookBook',
+    slug: 'CookBook',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff'
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0
+    updates: {
+      fallbackToCacheTimeout: 0
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#FFFFFF"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF'
       }
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    web: {
+      favicon: './assets/favicon.png'
     },
     extra: {
       apiKey: process.env.API_KEY,
       authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATA_BASE_URL,
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
