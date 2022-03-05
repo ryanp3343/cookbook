39c84fb9a0017e892b95958ad12bd32deed382be
joshuapopp17 <54725087+joshuapopp17@users.noreply.github.com>
2022-03-05 10:09:58 -0800

    updated

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,26 +1,54 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 
-const RecipeCard = ({name, directions, ingredients, url}) => {
+const RecipeCard = ({name, directions, ingredients, url, recipe}) => {
     const [directionList, setDirectionList] = useState([]);
+    const navigation = useNavigation();
     
+    const likeRecipe = () => {
+      alert("you liked " + recipe.Title)
+    }
+
+    const commentRecipe = () => {
+      alert("you commented " + recipe.Title)
+    }
+
+    const visitProfile = () => {
+      alert("you visited user for " + recipe.Title)
+    }
+
     return (
         <View style={styles.Card}>
+          <Pressable onPress={() => navigation.navigate('RecipeExpanded', {
+            name: recipe.Title,
+            directions: recipe.Directions,
+            photoURL: recipe.Url,
+            ingredients: recipe.Ingredients
+          })}>
             <View style={styles.Title}>
                 <Text style={styles.recipeTitle}>{name}</Text>
                 <Image source={{uri: url}}
-                       style={{width: 320, height: 320}}
+                       style={{width: "100%", height: 320}}
                  />
                  <View style={styles.directionsContainer}>
                     
                  </View>
             </View>
+          </Pressable>
             <View style={styles.buttonContainer}>
-                <Icon style={styles.icon} size={30} name="favorite" type='material' color='#000'/>
-                <Icon style={styles.icon} size={30} name="question-answer" type='material' color='#000'/>
-                <Icon style={styles.icon} size={30} name="person" type='material' color='#000'/>
+              <TouchableOpacity onPress={() => likeRecipe()}>
+                <Icon style={styles.icon} size={40} name="heart" type='feather' color='#000'/>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => commentRecipe()}>
+                <Icon style={styles.icon} size={40} name="message-square" type='feather' color='#000'/>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => visitProfile()}>
+                <Icon style={styles.icon} size={40} name="user" type='feather' color='#000'/>
+              </TouchableOpacity>
             </View>
         </View>
     )
@@ -30,17 +58,20 @@ const styles = StyleSheet.create({
   Card: {
     // backgroundColor: '#ddd',
     fontSize: 20,
-    marginBottom: 15,
+    marginBottom: 30,
     fontWeight: '600',
-    width: 340,
+    width: '100%',
     height: 'auto',
     borderRadius: 10,
-    padding: 10,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderColor: '#D8d8d8',
   },
   recipeTitle: {
     fontSize: 30,
     fontWeight: 'bold',
     marginBottom: 10,
+    paddingLeft: 10,
   },
   Title: {
     flexDirection: 'column',
@@ -67,10 +98,11 @@ const styles = StyleSheet.create({
   buttonContainer: {
       flexDirection: 'row',
       justifyContent: 'flex-start',
-      marginVertical: 10,
+      marginVertical: 5,
+      paddingLeft: 10,
   },
   icon: {
-      marginRight: 20,
+      marginRight: 25,
   },
   directionsContainer: {
       marginVertical: 10,
